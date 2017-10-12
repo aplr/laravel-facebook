171cceb49a7494db1304cee27e3a002fe61f7b3f
Andreas Pfurtscheller <andreas.pfurtscheller@gmail.com>
2017-10-12 13:40:53 +0200

    Fixed Facade naming conflicts

--- src/Facade.php
+++ src/Facade.php
@@ -3,9 +3,9 @@
 namespace Aplr\LaravelFacebook;
 
 use Facebook\Facebook;
-use Illuminate\Support\Facades\Facade as Facade;
+use Illuminate\Support\Facades\Facade as BaseFacade;
 
-class Facebook extends Facade {
+class Facade extends BaseFacade {
     
     /**
      * Get the registered component.
